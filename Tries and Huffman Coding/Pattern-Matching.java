3da90f3cfb6bcc95fe05dbf4f4d76442be807cf9
Samarth Jain <56931905+samarthjain10@users.noreply.github.com>
2020-02-02 20:45:10 +0530

    Add files via upload

---
+++ Tries and Huffman Coding/Pattern-Matching.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+
+class TrieNode{
+	char data;
+	boolean isTerminating;
+	TrieNode children[];
+	int childCount;
+
+	public TrieNode(char data) {
+		this.data = data;
+		isTerminating = true;
+		children = new TrieNode[26];
+		childCount = 0;
+	}
+}
+
+public class Trie {
+	private TrieNode root;
+	
+	public Trie() {
+		root = new TrieNode('\0');
+	}
+
+	private void add(TrieNode root, String word){
+		if(word.length() == 0){
+			root.isTerminating = true;
+			return;
+		}		
+		int childIndex = word.charAt(0) - 'a';
+		TrieNode child = root.children[childIndex];
+		if(child == null){
+			child = new TrieNode(word.charAt(0));
+			root.children[childIndex] = child;
+			root.childCount++;
+		}
+		add(child, word.substring(1));
+	}
+
+	public void add(String word){
+		add(root, word);
+	}
+	
+	
+	public boolean patternMatching(ArrayList<String> input, String pattern) 
+    {
+        for(int i=0;i<input.size();i++)
+        {
+            for(int j=0;j<input.get(i).length();j++)
+            {
+                add(input.get(i).substring(j));
+            }
+        }
+        return search(pattern);
+	}
+    public boolean search(String word)
+    {
+        return search(root,word);
+    }
+    public boolean search(TrieNode root,String word)
+    {
+        if(word.length()==0)
+            return root.isTerminating;
+        int index=word.charAt(0)-'a';
+        TrieNode child=root.children[index];
+        if(child==null)
+            return false;
+        return search(child,word.substring(1));
+    }
+}
